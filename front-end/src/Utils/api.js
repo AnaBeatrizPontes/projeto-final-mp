ba966e5229918d26d4b975537aec35609812fe96
Hyago Gabriel <oicueio616@gmail.com>
2021-04-27 00:07:29 -0300

    Padronização do código atual para passar no eslint

--- front-end/src/Utils/api.js
+++ front-end/src/Utils/api.js
@@ -1,29 +1,28 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL:
-        "http://localhost:8080/v1/api/",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: 'http://localhost:8080/v1/api/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use(
-    (config) => {
-        if (!config.url.endsWith("/auth")) {
-            config.headers["Authorization"] = localStorage.getItem("token");
-        }
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
+  (config) => {
+    if (!config.url.endsWith('/auth')) {
+      config.headers['Authorization'] = localStorage.getItem('token');
     }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 export const login = (email, password) => {
-    return api.post('/auth', { email, password });
+  return api.post('/auth', { email, password });
 };
 
 export const register = (email, password, firstName, lastName) => {
-    return api.post('/user', { email, password, firstName, lastName });
-}
+  return api.post('/user', { email, password, firstName, lastName });
+};
--- front-end/src/pages/Root.js
+++ front-end/src/pages/Root.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
-import PagesLogin from './Login/Login'
-import PagesRegister from './Register/Register'
-import PagesHome from './Home/Home'
-import PagesNotFound from './NotFound'
-import PrivateRoute from './PrivateRoute'
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PagesLogin from './Login/Login';
+import PagesRegister from './Register/Register';
+import PagesHome from './Home/Home';
+import PagesNotFound from './NotFound';
+import PrivateRoute from './PrivateRoute';
 
 const Root = () => {
-    return (
-        <Router>
-            <Switch>
-                <PrivateRoute exact path="/home" component={PagesHome} />
-                <Route exact path="/register" component={PagesRegister} />
-                <Route exact path="/" component={PagesLogin} />
-                <PrivateRoute component={PagesNotFound} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute exact path="/home" component={PagesHome} />
+        <Route exact path="/register" component={PagesRegister} />
+        <Route exact path="/" component={PagesLogin} />
+        <PrivateRoute component={PagesNotFound} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default Root;
