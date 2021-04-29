64bbc4c1d8a1e50a92e83fa772c9547fd803d334
joaopedroazul <joaopedroandrade@aluno.unb.br>
2021-04-28 23:35:03 -0300

    back_end: Login funcionando, Crud falta o update

--- front-end/src/components/Opcoes/Opcoes.js
+++ front-end/src/components/Opcoes/Opcoes.js
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 import style from './Opcoes.module.css'
 export default function Opcoes(){
     return(
         <div className={style.container}>
+=======
+import './Opcoes.css';
+export default function Opcoes() {
+    return (
+        <div className="container">
+>>>>>>> 6b3c6abbb92d3da16cbc80bb238199ecf09db92a
             <h1>oiiiiiiiiiiiiii</h1>
-            <hr/>
+            <hr />
         </div>
     );
 }
