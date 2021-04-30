00c8e345dbbe6e0e292bc55a0afd5c198edc1468
Rafael Barbosa <rafael.1940.b@gmail.com>
2021-04-30 20:10:28 -0300

    Cria pagina de resposta de questionarios

--- front-end/src/Utils/api.js
+++ front-end/src/Utils/api.js
@@ -26,3 +26,84 @@ export const login = (email, password) => {
 export const register = (email, password, firstName) => {
   return api.post('/users', { email, password, firstName });
 };
+
+export const getQuestionario = (questionarioId) => {
+  // return api.get(`/questionario/${questionarioId}`);
+  return {
+    titulo: "Questionario de teste",
+    descricao: "Este é um questionário de testes que eu criei para testes. Estou apenas testando!!",
+    userName: "rafael.barbosa",
+    link: "???",
+    perguntas: [
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Sim ou não?",
+        tipo: "multiplaEsc",
+        dados: [
+          {
+            opcao: "Sim",
+            valor: "s"
+          },
+          {
+            opcao: "Não",
+            valor: "n"
+          },
+        ]
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+      {
+        descricao: "Você gosta?",
+        tipo: "respCurta",
+        dados: [],
+      },
+    ],
+  };
+};
